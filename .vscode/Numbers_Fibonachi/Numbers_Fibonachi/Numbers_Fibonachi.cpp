bb1d212e839ac0b8929fd0d189431ac69eb43952
Borys <borisbilkevich@gmail.com>
2020-09-03 01:21:57 +0300

    BorysBB_Change

---
+++ .vscode/Numbers_Fibonachi/Numbers_Fibonachi/Numbers_Fibonachi.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Enter range of number start from 1 till : ";//<< endl - new line
+    int sum;
+    cin >> sum;
+    int start = 1;
+
+    for (int end = 1; end <= sum; end += start) {
+        cout << end << endl;
+        start = end - start;
+    }
+    return 0;
+}
